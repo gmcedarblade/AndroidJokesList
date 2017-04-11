d024739f82aef751ad0b69a434ed22cea32860c0
Greg Cedarblade <gmcedarblade@gmail.com>
2017-04-11 09:32:56 -0500

    added more layouts to MainActivity

--- app/src/main/java/edu/cvtc/android/jokelist/MainActivity.java
+++ app/src/main/java/edu/cvtc/android/jokelist/MainActivity.java
@@ -1,12 +1,34 @@
 package edu.cvtc.android.jokelist;
 
+import android.graphics.Color;
+import android.support.v4.view.ViewGroupCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Data Structure for the List of Joke object the Activity will present to the user.
+     */
+    private ArrayList<Joke> jokeList = new ArrayList<>();
+
+    /**
+     * Layout container for a Joke.
+     */
+    private LinearLayout jokeLayout;
+
+    /**
+     * ScrollView used to hold the Jokes in jokeList.
+     */
+    private ScrollView jokeScrollView;
 
     /**
      * EditText used to enter text for new Joke to be added to jokeList.
@@ -21,9 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         initLayout();
+
     }
 
     /**
@@ -38,7 +61,29 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout addJokeLayout = new LinearLayout(this);
         addJokeLayout.setOrientation(LinearLayout.HORIZONTAL);
 
+        addJokeButton = new Button(this);
+        addJokeButton.setText("Add Joke");
+        addJokeButton.setTextColor(Color.WHITE);
+        addJokeButton.setBackgroundColor(Color.DKGRAY);
+        addJokeLayout.addView(addJokeButton);
 
+        // Create our LayoutParams for EditText
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        jokeEditText = new EditText(this);
+        jokeEditText.setLayoutParams(params);
+        addJokeLayout.addView(jokeEditText);
+
+        jokeLayout = new LinearLayout(this);
+        jokeLayout.setOrientation(LinearLayout.VERTICAL);
+
+        jokeScrollView = new ScrollView(this);
+        jokeScrollView.addView(jokeLayout);
+
+        container.addView(addJokeLayout);
+        container.addView(jokeScrollView);
 
         setContentView(container);
 
